f633a8280f3971a2daa32f2455b38ce0577e48c7
SerialEditor <81110169+SerialEditor@users.noreply.github.com>
2022-06-14 01:14:31 +0400

    Update popups-by-js.js

--- popups-by-js.js
+++ popups-by-js.js
@@ -3,14 +3,6 @@ let popups = document.querySelectorAll('div');
 for (let popup of popups) {
   popup.onclick = function() {
     popup.textContent = '3';
-    let closedButton = document.createElement('button');
-    popup.append(closedButton);
-    closedButton.onclick = function() {
-      if (popup.textContent == '3') {
-       popup.textContent = '';
-       this.remove();
-      }
-    }
   }
 };
 
