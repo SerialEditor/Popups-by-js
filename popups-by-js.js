92577324c1006343ec32e9a3ec4e73c6dc130d3c
SerialEditor <81110169+SerialEditor@users.noreply.github.com>
2022-06-19 02:23:49 +0400

    Update popups-by-js.js

--- popups-by-js.js
+++ popups-by-js.js
@@ -2,8 +2,10 @@
 let popups = document.querySelectorAll('.image-pointer');
 let imageWrapper = document.querySelector('.image-wrapper');
 let innerImage = document.querySelector('.inner-image');
+let gentleHint = document.querySelector('.prompt');
 for (let popup of popups) {
   popup.onclick = function() {
+    gentleHint.textContent = 'Кликни снова, чтобы скрыть';
     if (!popup.contains(imageWrapper)) {
       innerImage.src = popup.dataset.image;
       popup.append(imageWrapper);
