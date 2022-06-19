1edff1636f6ce3e4ec193b7678cfecba30533262
SerialEditor <81110169+SerialEditor@users.noreply.github.com>
2022-06-19 17:21:01 +0400

    Update popups-by-js.js

--- popups-by-js.js
+++ popups-by-js.js
@@ -2,18 +2,22 @@
 let popups = document.querySelectorAll('.image-pointer');
 let imageWrapper = document.querySelector('.image-wrapper');
 let innerImage = document.querySelector('.inner-image');
+let closingButton = document.querySelector('.closing-button');
 let gentleHint = document.querySelector('.prompt');
 for (let popup of popups) {
   popup.onclick = function() {
-    gentleHint.textContent = 'Кликни снова, чтобы скрыть.';
+    gentleHint.textContent = 'Кликни по кнопке, чтобы скрыть.';
     if (!popup.contains(imageWrapper)) {
       innerImage.src = popup.dataset.image;
       popup.append(imageWrapper);
-      if (!imageWrapper.classList.contains('visualised')) {
-        imageWrapper.classList.add('visualised'); 
-      } 
-    } else {imageWrapper.classList.toggle('visualised');}
+      imageWrapper.classList.add('visualised'); 
+    } else if (popup.contains(imageWrapper) && !imageWrapper.classList.contains('visualised')) {
+      imageWrapper.classList.add('visualised');
+    }
   }
+};
+closingButton.onclick = function() {
+  imageWrapper.classList.remove('visualised');
 };
 
 
