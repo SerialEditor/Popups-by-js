1b2e89ffce28649bd66e27f9092e4a44859d77db
SerialEditor <81110169+SerialEditor@users.noreply.github.com>
2022-06-21 19:48:06 +0400

    Update popups-by-js.js

--- popups-by-js.js
+++ popups-by-js.js
@@ -1,15 +1,16 @@
 'use strict';
-let popups = document.querySelectorAll('.image-pointer');
+let pointers = document.querySelectorAll('.image-pointer');
+let containers = document.querySelectorAll('.inner-container');
 let imageWrapper = document.querySelector('.image-wrapper');
 let innerImage = document.querySelector('.inner-image');
 let pageLink = document.querySelector('.page-link');
 let gentleHint = document.querySelector('.gentle-hint');
-for (let popup of popups) {
-  popup.onclick = function() {
+for (let pointer of pointers) {
+  pointer.onclick = function() {
     gentleHint.textContent = 'Кликни снова, чтобы скрыть.';
-    if (!popup.contains(imageWrapper)) {
-      innerImage.src = popup.dataset.image;
-      popup.append(imageWrapper);
+    if (!pointer.contains(imageWrapper)) {
+      innerImage.src = pointer.dataset.image;
+      pointer.append(imageWrapper);
       if (!imageWrapper.classList.contains('visualised')) {
         imageWrapper.classList.add('visualised');
         pageLink.classList.add('visualised');
