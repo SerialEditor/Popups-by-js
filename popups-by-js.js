1cbe5b7a89bc9013cd8a31cbd2820e7b4e8419bd
SerialEditor <81110169+SerialEditor@users.noreply.github.com>
2022-07-17 00:00:50 +0400

    Update popups-by-js.js

--- popups-by-js.js
+++ popups-by-js.js
@@ -1,27 +1,24 @@
 'use strict';
 let pointers = document.querySelectorAll('.image-pointer');
 let innerImage = document.querySelector('.inner-image');
-let pageLink = document.querySelector('.page-link');
-let gentleHint = document.querySelector('.gentle-hint');
-let startPhrase = gentleHint.textContent;
+let pageLink = document.querySelector('.page-link')
 
 function updateDisplay() {
-  gentleHint.textContent = startPhrase;
   if (pageLink.classList.contains('visualised')) {
     pageLink.classList.remove('visualised');
   }
 };
 
-window.addEventListener('click', e => {
+/* window.addEventListener('click', e => {
   const target = e.target;
   if (!target.closest('.image-pointer')) {
     gentleHint.textContent = 'Да не сюда!';
   }
 }) 
+*/
 
 for (let pointer of pointers) {
   pointer.onclick = function() {
-    gentleHint.textContent = 'Кликни снова, чтобы скрыть.';
     if (!pointer.contains(pageLink)) {
       innerImage.src = pointer.dataset.image;
       pointer.append(pageLink);
