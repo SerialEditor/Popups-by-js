6934696a3439421bece0548ff40b9100e13313f3
SerialEditor <81110169+SerialEditor@users.noreply.github.com>
2022-07-21 13:02:42 +0400

    Update popups-by-js.js

--- popups-by-js.js
+++ popups-by-js.js
@@ -27,7 +27,7 @@ for (let pointer of pointers) {
         pageLink.classList.add('visualised');
       }
     } else {pageLink.classList.toggle('visualised');}
-    rightValue.textContent = pageLink.getBoundingClientRect().right;
+    rightValue.textContent = Math.trunc(pageLink.getBoundingClientRect().right);
   }
 };
 
