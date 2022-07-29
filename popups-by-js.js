c2f36813800631e3e0e182cfadd03ec1c1bfeba3
SerialEditor <81110169+SerialEditor@users.noreply.github.com>
2022-07-30 01:27:15 +0400

    Update popups-by-js.js

--- popups-by-js.js
+++ popups-by-js.js
@@ -11,7 +11,7 @@ function correctPageLinkPosition() {
   const diff = getDocumentDimensionsDiff();
   if (diff) {
     pageLink.style.left = diff + 'px';
-  } else {pageLink.style.left = 50 + '%';}
+  } else {pageLink.style.left = 0;}
 }
 
 function clearDisplay() {
